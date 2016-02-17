dc150885bc7fba4f4657797018b9bc89f5e91d08
Luka Furlan <luka@tray.io>
2016-02-17 16:01:42 +0000

    add sample config

---
+++ config.hcl
@@ -0,0 +1,10 @@
+# nginx-production group
+"nginx-production" {
+  count = 3
+  age = 72 # 3 days
+}
+
+"nginx-staging" {
+  count = 1
+  age = 5
+}
---
+++ config.js
@@ -0,0 +1,11 @@
+{
+    "nginx-production": {
+        "count": 3,
+        "age": 72
+    },
+
+    "nginx-staging": {
+        "count": 1,
+        "age": 5
+    }
+}
