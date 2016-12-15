54e7b196d7cb6bcfad87108dd4b60257ffd5c82f
Luka Furlan <luka@tray.io>
2016-12-15 15:16:28 +0000

    config validation

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"io/ioutil"
 
 	"github.com/hashicorp/hcl"
@@ -8,6 +9,7 @@ import (
 
 // Count: how many instances
 // Age: minium age of instance
+// Region: AWS region
 type ConfigItem struct {
 	Count  int
 	Age    int
@@ -22,7 +24,30 @@ func New(filename string) (Configuration, error) {
 		return nil, err
 	}
 
-	return generate(data)
+	cfg, err := generate(data)
+	if err != nil {
+		return cfg, err
+	}
+
+	return validate(cfg)
+}
+
+func validate(config Configuration) (Configuration, error) {
+	for group, cfg := range config {
+		if cfg.Count <= 0 {
+			return config, fmt.Errorf("%s: count cannot be zero or negative value", group)
+		}
+
+		if cfg.Age <= 0 {
+			return config, fmt.Errorf("%s: age cannot be zero or negative value", group)
+		}
+
+		if cfg.Region == "" {
+			return config, fmt.Errorf("%s: invalid region", group)
+		}
+	}
+
+	return config, nil
 }
 
 func generate(data []byte) (Configuration, error) {
